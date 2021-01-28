79c757df7bb7fd310035efee0ac4ac24a4517771
jamieTardi <jamie.tardi@gmail.com>
2021-01-28 17:11:46 +0000

    media query additions

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,7 +1,3 @@
 import Commerce from '@chec/commerce.js';
 
-export const commerce = new Commerce(
-	'pk_2237088ff10278fa2d3dc46cb84c58d60ada3f4f6f0f2',
-
-	true,
-);
+export const commerce = new Commerce(process.env.REACT_APP_COMMERCE_KEY, true);
