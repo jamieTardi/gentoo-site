678c5ee6c13f7577184db115d063edce5c31cc20
jamieTardi <jamie.tardi@gmail.com>
2021-02-01 15:50:20 +0000

    scalability fix on parallax

--- src/pages/ShopHome.jsx
+++ src/pages/ShopHome.jsx
@@ -30,7 +30,7 @@ const ShopHome = () => {
 	];
 	return (
 		<>
-			<div className='main-and-section'>
+			<div className='main-and-text1'>
 				<div className='pimg1'></div>
 				<div className='ptext'>
 					<span className='border'>Purifiers</span>
@@ -67,20 +67,21 @@ const ShopHome = () => {
 				</p>
 			</section>
 
-			<div className='pimg2'></div>
-			<div className='ptext2'>
-				<span className='border'>Cooling</span>
-				<h5>
-					Click below to go to our shop for cooling fans, in here you will be
-					able to see a selection of our best air circulation systems and all of
-					the technical specs associated with them.
-				</h5>
-				<Link to='/fans'>
-					<button className='shop-btn'>Shop Now</button>
-				</Link>
-				<button className='shop-btn tech'>Technical Specs</button>
+			<div className='main-and-text2'>
+				<div className='pimg2'></div>
+				<div className='ptext2'>
+					<span className='border'>Cooling</span>
+					<h5>
+						Click below to go to our shop for cooling fans, in here you will be
+						able to see a selection of our best air circulation systems and all
+						of the technical specs associated with them.
+					</h5>
+					<Link to='/fans'>
+						<button className='shop-btn'>Shop Now</button>
+					</Link>
+					<button className='shop-btn tech'>Technical Specs</button>
+				</div>
 			</div>
-
 			<section className='section'>
 				<h2>Air Shower Fans</h2>
 				<p>
@@ -93,18 +94,20 @@ const ShopHome = () => {
 				</p>
 			</section>
 
-			<div className='pimg3'></div>
-			<div className='ptext3'>
-				<span className='border'>Humidifiers</span>
-				<h5>
-					Click below to go to our shop for humidifiers, in here you will be
-					able to see a selection of our best humidifiers and all of the
-					technical specs associated with them.
-				</h5>
-				<Link to='/humidifiers'>
-					<button className='shop-btn'>Shop Now</button>
-				</Link>
-				<button className='shop-btn tech'>Technical Specs</button>
+			<div className='main-and-text2'>
+				<div className='pimg3'></div>
+				<div className='ptext3'>
+					<span className='border'>Humidifiers</span>
+					<h5>
+						Click below to go to our shop for humidifiers, in here you will be
+						able to see a selection of our best humidifiers and all of the
+						technical specs associated with them.
+					</h5>
+					<Link to='/humidifiers'>
+						<button className='shop-btn'>Shop Now</button>
+					</Link>
+					<button className='shop-btn tech'>Technical Specs</button>
+				</div>
 			</div>
 
 			<section className='section'>
@@ -124,17 +127,19 @@ const ShopHome = () => {
 				</p>
 			</section>
 
-			<div className='pimg4'></div>
-			<div className='ptext4'>
-				<span className='border'>Heaters</span>
-				<h5>
-					Our selection of heaters will be coming soon! Check back here shortly
-					for further details.
-				</h5>
-				<Link to='/'>
-					<button className='shop-btn'>Shop Now</button>
-				</Link>
-				<button className='shop-btn tech'>Technical Specs</button>
+			<div className='main-and-text2'>
+				<div className='pimg4'></div>
+				<div className='ptext4'>
+					<span className='border'>Heaters</span>
+					<h5>
+						Our selection of heaters will be coming soon! Check back here
+						shortly for further details.
+					</h5>
+					<Link to='/'>
+						<button className='shop-btn'>Shop Now</button>
+					</Link>
+					<button className='shop-btn tech'>Technical Specs</button>
+				</div>
 			</div>
 		</>
 	);
