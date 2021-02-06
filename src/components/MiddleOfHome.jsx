c725f47d4c7d3d805a14e5af223e9339307d8b8d
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-02-06 14:22:49 +0000

    added overlays to images on home, corrected media queries

--- src/components/MiddleOfHome.jsx
+++ src/components/MiddleOfHome.jsx
@@ -8,13 +8,29 @@ import {
 
 const MiddleOfHome = () => {
 	const [toggleOverlay, setToggleOverlay] = useState(true);
+	const [toggleHumidOverlay, setToggleHumidOverlay] = useState(true);
+	const [togglePurifierOverlay, setTogglePurifierOverlay] = useState(true);
 	const [purifierMain, setPurifierMain] = useState(true);
 	const [fanMain, setFanMain] = useState(false);
 	const [humidMain, setHumidMain] = useState(false);
 
+	//Toggle functions for the on click event on thumbnails
 	const fanToggle = () => {
 		setPurifierMain(false);
 		setFanMain(true);
+		setHumidMain(false);
+	};
+
+	const humidToggle = () => {
+		setPurifierMain(false);
+		setFanMain(false);
+		setHumidMain(true);
+	};
+
+	const purifierToggle = () => {
+		setPurifierMain(true);
+		setFanMain(false);
+		setHumidMain(false);
 	};
 	return (
 		<div className='middle-container'>
@@ -44,13 +60,34 @@ const MiddleOfHome = () => {
 						Cooling
 					</button>
 				</div>
-				<div className='thumbnail-image'></div>
-				<div className='overlay2'>
-					<button>Humidifiers</button>
+				<div
+					className='thumbnail-image thumbnail2'
+					onMouseEnter={() => {
+						setToggleHumidOverlay(false);
+					}}></div>
+				<div
+					className={toggleHumidOverlay ? 'hide' : 'overlay2'}
+					onMouseLeave={() => {
+						setToggleHumidOverlay(true);
+					}}>
+					<button
+						onClick={() => {
+							humidToggle();
+						}}>
+						Humidifiers
+					</button>
 				</div>
-				<div className='thumbnail-image'></div>
-				<div className='overlay3'>
-					<button>Cooling</button>
+				<div
+					className='thumbnail-image thumbnail3'
+					onMouseEnter={() => {
+						setTogglePurifierOverlay(false);
+					}}></div>
+				<div
+					className={togglePurifierOverlay ? 'hide' : 'overlay3'}
+					onMouseLeave={() => {
+						setTogglePurifierOverlay(true);
+					}}>
+					<button onClick={purifierToggle}>Purifiers</button>
 				</div>
 			</div>
 		</div>
